4207ed4ca76f6e226cadcf21896f095f35fd3f6b
IBNJangoViti <IBNalewabau@outlook.com>
2025-02-08 23:55:54 +0000

    Changes

--- info.php
+++ info.php
@@ -1,19 +1,15 @@
-    <!doctype html>
-    <html>
-      <head>
-        <title>Document title</title>
-      </head>
-      <body style="background-color:black;">
-         <center>
-           <img src="https://www.mywebsite.com/logo_banner.png">
-           <br>
-            </center>
-            <br>
-            <h1 style="color:white";>About Us</h1>
-            <br>
-            <p style='color:white";>A little about us...</p>
-            <hr>
-         </body>
-    </html>
+<!doctype html>
+<html>
+ <head>
+      <title>Document title</title>
+ </head>
+
+ <body>
+  <h1> IBNSPORTS </h1>     
+ </body>
+
+
+
+</html>
 
 
