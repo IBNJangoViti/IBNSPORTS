fc70e848ebf12cc517686268d1f61c1fa3f2f690
IBNJangoViti <IBNalewabau@outlook.com>
2025-02-09 10:47:17 +0000

    New files

--- info.php
+++ info.php
@@ -1,15 +1,31 @@
 <!doctype html>
 <html>
  <head>
-      <title>Document title</title>
+      <title>IBN Sports Hub</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="CSS/main.css">
  </head>
 
- <body>
-  <h1> IBNSPORTS </h1>     
- </body>
+<body>
 
 
-
+<div class="container">
+	<img src="images/logo.png">
+	<h1>Welcome to IBNSPORTS</h1>
+	<p>Explore the Duke of York Royal Military School's Sports by selecting one below:</p>
+	
+	<div class="sports-buttons">
+		
+		<a href="rugby.php"> RUGBY</a>
+		<a href="rugby.php">NETBALL</a>
+		<a href="rugby.php">HOCKEY</a>
+		<a href="rugby.php">CRICKET</a>
+	</div>
+</div>
+		
+		
+</body>
 </html>
 
 
